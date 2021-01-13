b7f859f66bdb72a42d4116fec3d21c2fe3f0f85f
LaurenStanton <62582002+LaurenStanton@users.noreply.github.com>
2021-01-12 23:56:50 -0500

    Update bat_cleaning.R

--- data_prep_scripts/bat_cleaning.R
+++ data_prep_scripts/bat_cleaning.R
@@ -20,3 +20,46 @@ write_bat_edgelist_tofile <- function(hour_list){
 }
 
 write_bat_edgelist_tofile(net.list)
+
+
+# loop for generating multiple networks
+# subset by hour, create an igraph for each hour, calculate metrics for each network hour
+
+g <- read.table("bats_byhour_edgelist.csv", header = TRUE, sep = ",")
+
+keys<-unique(g$hour)
+
+new_df<-do.call(bind_rows, lapply(1:length(keys), function(i){
+  print(paste0(i," of ",length(keys)))
+  #subset out each hour
+  temp<- g %>% 
+    filter(hour==keys[i])
+  #make igraph object
+  g2<-graph.data.frame(temp) #this is the igraph object
+  #compute metrics (e.g., density)
+  g3<- length(V(g2))
+  g4<-igraph::edge_density(g2)
+  g5<-mean(igraph::eigen_centrality(g2, directed = FALSE)$vector)
+  g6<-mean(igraph::betweenness(g2, directed = FALSE))
+  g7<-igraph::mean_distance(g2)
+  g8 <- effective_information(g2, effectiveness = FALSE)
+  g9 <- g8/log2(g3) # normalized value to control for network size 
+  #write igraph object to file so that we can plot network later for each hour
+  save(g2, file=paste0("/Users/laurenstanton/Desktop/CNWW/Bats/igraph_objects_4_plotting/","hour_",keys[i],".RData"))
+  #create summary dataframe
+  dat<-data.frame(hour=keys[i], n_nodes =g3, density=g4, mean_eignvector_centrality=g5, mean_betweenness_centrality=g6, average.path.length = g7, effective_information=g8,ie_adjusted=g9)
+return(dat)
+}))
+
+write.table(new_df,file="/Users/laurenstanton/Desktop/CNWW/Bats/bat_metrics.csv",sep=",", row.names=FALSE)
+
+#to plot all 67 networks
+for(i in 1:length(keys)){
+  load(paste0("/Users/laurenstanton/Desktop/CNWW/Bats/igraph_objects_4_plotting/","hour_",keys[i],".RData"))
+  plot(g2) #need to figure out what parameters to use for plotting networks - LAS
+}
+
+#to plot one at a time by changing value of i
+i=15
+load(paste0("/Users/laurenstanton/Desktop/CNWW/Bats/igraph_objects_4_plotting/","hour_",keys[i],".RData"))
+plot(g2)  # need to figure out what parameters to use for plotting networks - LAS
